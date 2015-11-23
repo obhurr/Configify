da8c4e6585db8e6547a30f061c5073b36236e773
denny-hendrix <hendrix.denny@gmail.com>
2015-11-23 17:10:22 -0600

    Created ConfigurationPriceCalculator

---
+++ src/Configify/ConfigurationPriceCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configify
+{
+    public class ConfigurationPriceCalculator
+    {
+        public decimal Calculate(Configuration configuration)
+        {
+            var price = configuration.Price;
+
+            foreach (var configurationItem in configuration.ConfigurationItems)
+            {
+                price += configurationItem.Price;
+
+                price += configurationItem.ConfigurationItemOptions
+                    .Where(i => i.IsSelected)
+                    .Sum(configurationItemOption => configurationItemOption.Price);
+            }
+            return price;
+        }
+    }
+}
---
+++ test/Configify.Test/ConfigurationPriceCalculatorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Configify.Test
+{
+    [TestFixture]
+    public class ConfigurationPriceCalculatorTests
+    {
+        [Test]
+        public void Does_Calculate_Correctly()
+        {
+            var config = new Configuration
+            {
+                Price = 1.00m
+            };
+
+            var configItem = new ConfigurationItem
+            {
+                Price = .25m
+            };
+
+            configItem.ConfigurationItemOptions.Add(new ConfigurationItemOption
+            {
+                IsSelected =  true,
+                Price = 3.10m
+            });
+
+            configItem.ConfigurationItemOptions.Add(new ConfigurationItemOption
+            {
+                IsSelected = false,
+                Price = 3.10m
+            });
+
+            config.ConfigurationItems.Add(configItem);
+
+            var calculator = new ConfigurationPriceCalculator();
+            var result = calculator.Calculate(config);
+
+            Assert.AreEqual(4.35m, result);
+        }
+    }
+}
