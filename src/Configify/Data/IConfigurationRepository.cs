01ff89e8f2850e2802a6ac95ba1a2ae80dbf0aae
denny-hendrix <hendrix.denny@gmail.com>
2015-12-02 12:09:39 -0600

    Added configuration repository interface

---
+++ src/Configify/Data/IConfigurationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configify.Data
+{
+    public interface IConfigurationRepository
+    {
+        IEnumerable<Configuration> GetConfigurations();
+        Configuration GetConfigurationById(Guid id);
+    }
+}
